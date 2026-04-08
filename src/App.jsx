dcd273c2ea89d41cdd8c0eb6b37f82559330e86d
Jorge Sanchez <jsanchez.contact29@gmail.com>
2026-04-08 19:33:12 +0200

    Make Splash appear before Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react"
 import Auth from "./components/Auth/Auth"
 import Splash from "./components/Splash"
 
 function App() {
+  const [loading, setLoading] = useState(true)
 
-  return (
-    <>
-      <Auth />
-    </>
-  )
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+    return () => clearTimeout(timer);
+  },[])
+
+  if(loading) return <Splash/>
+
+  return <Auth/>
 }
 
 export default App

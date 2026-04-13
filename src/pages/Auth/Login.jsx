22198d9681a87f628e2f3810a98bfcc1cb64a8c9
Jorge Sanchez <jsanchez.contact29@gmail.com>
2026-04-13 10:31:18 +0200

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,16 +1,49 @@
 import { useNavigate } from "react-router-dom"
 import { FormBackground, CustomButton, CustomClicableText } from "../../components"
+import { useState } from "react"
+
 
 function FillLogin() {
     const navigate = useNavigate()
+
+    const [userName, setuserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async () => {
+        if (!userName) return alert("Fill in the user field")
+        if (!password) return alert("Fill in the password field")
+
+        const newUser = {
+            username: userName,
+            password: password
+        }
+
+        try {
+            const response = await fetch('http://81.0.55.211:8081/api/auth/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newUser),
+            });
+
+            const result = await response.json();
+            console.log('Respuesta del servidor:', result);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
     return (
         <>
             <p>User: </p>
-            <input type="text" className="cust-input" placeholder="user1234"></input>
+            <input type="text" className="cust-input" value={userName}
+                onChange={(e) => setuserName(e.target.value)}
+                placeholder="user1234" ></input>
             <p>Password: </p>
-            <input type="password" className="cust-input" placeholder="******"></input>
+            <input type="password" className="cust-input" value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="******"></input>
             <CustomButton
-                onClick={null}   // cambiar luego
+                onClick={handleSubmit}   // cambiar luego
                 className="text-green-dark bg-green-solid p-2 form-element rounded"
                 text="Log In"
                 type="submit" />
@@ -29,7 +62,7 @@ function FillLogin() {
 
 function Login() {
     return (
-        <FormBackground body={<FillLogin/>} title="Welcome"></FormBackground>
+        <FormBackground body={<FillLogin />} title="Welcome"></FormBackground>
     )
 }
 
